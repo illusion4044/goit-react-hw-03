63a3e0a6f6671d0ce43f3a1f82b2dbe96f3c3b93
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-06-26 15:58:13 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,16 +2,22 @@
 import css from "./ContactForm.module.css";
 
 export default function ContactForm() {
-  
-
-  
+const handleSubmit = (event) => {
+    event.preventDefault();
+console.log("handleSybmit");
+};
 
     return (
-        <form className={css.form} >
+        <form className={css.form} onSubmit={handleSubmit} >
             <div className={css.group}>
                 <label className={css.label} >Username:</label>
                 <input className={css.input} type="text" name="username"  />
             </div>
+            <div className={css.group}>
+                <label className={css.label} >Number:</label>
+                <input className={css.input} type="text" name="number"  />
+            </div>
+            <button type="submit">Add contact</button>
         </form>
     );
 }
