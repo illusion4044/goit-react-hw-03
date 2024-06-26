53d6858a698ae8f6683c085208e90da2aaaac9c3
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-06-26 14:49:26 +0300

    css and minor updates

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,25 +1,16 @@
-import { useId } from "react";
+// import { useId } from "react";
+import css from "./ContactForm.module.css";
 
-export default function ContactForm({ onAdd }) {
-    const id = useId();
+export default function ContactForm() {
+  
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const form = event.target;
-
-        onAdd({
-          username: form.elements.username.value,
-          role: form.elements.role.value,
-        });
-    
-        form.reset();
-    };
+  
 
     return (
-        <form  onSubmit={handleSubmit}>
-            <div >
-                <label htmlFor={`username-${id}`}>Username:</label>
-                <input type="text" name="username" id={`username-${id}`} />
+        <form className={css.form} >
+            <div className={css.group}>
+                <label className={css.label} >Username:</label>
+                <input className={css.input} type="text" name="username"  />
             </div>
         </form>
     );
